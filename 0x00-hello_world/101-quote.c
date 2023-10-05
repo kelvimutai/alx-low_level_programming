a1b8d1f5a9cd88c0e5d71a7c670b0b35a7ca86ae
kelvimutai <kelvimutai@gmail.com>
2023-10-05 06:01:01 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - A C program that prints a line to the standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return (1);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
-
