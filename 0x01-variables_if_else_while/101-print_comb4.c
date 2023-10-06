e99f051d958b76979be35052251689d97dd3c4d6
kelvimutai <kelvimutai@gmail.com>
2023-10-06 12:38:48 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  Program prints  possible different combinations of three
+ * digits in ascending order, separated by ", ".
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+
+				if (!(i == 7 && j == 8 && k == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, num2;
+
+    for (num1 = 0; num1 <= 99; num1++)
+    {
+        for (num2 = num1; num2 <= 99; num2++)
+        {
+            putchar('0' + num1 / 10);
+            putchar('0' + num1 % 10);
+
+            putchar(' ');
+
+            putchar('0' + num2 / 10);
+            putchar('0' + num2 % 10);
+
+            if (num1 != 99 || num2 != 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    return (0);
+}
