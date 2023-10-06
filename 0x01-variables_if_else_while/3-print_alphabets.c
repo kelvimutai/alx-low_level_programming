bd1a9170675138c12f659ce329b62d5d529023bd
kelvimutai <kelvimutai@gmail.com>
2023-10-06 01:06:30 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet followed by uppercase,
+ * and a newline using putchar only three times.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char c = 'a';
+    char d = 'A';
+
+    while (c <= 'z')
+    {
+        putchar(c);
+        c++;
+    }
+
+    while (d <= 'Z')
+    {
+        putchar(d);
+        d++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
