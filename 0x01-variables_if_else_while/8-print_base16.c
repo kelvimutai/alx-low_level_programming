34529c5a1e89d6fddc45d2f39b531eb1c534c3b7
kelvimutai <kelvimutai@gmail.com>
2023-10-06 10:41:27 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This function prints numbers in base 16.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar('0' + i);
+		}
+		else
+		{
+			putchar('a' + (i - 10));
+		}
+		putchar('\n');
+	}
+
+	return (0);
+}
