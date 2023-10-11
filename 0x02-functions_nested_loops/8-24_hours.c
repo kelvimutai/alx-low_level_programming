5a2769eeb5680cb0ca38b5a8aad56a7c5051e824
kelvimutai <kelvimutai@gmail.com>
2023-10-11 01:36:37 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer.
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			printf("%.2d:%.2d\n", hour, minute);
+		}
+	}
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
