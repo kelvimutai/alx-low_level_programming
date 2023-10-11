f916fd28256752825c6fbf428102f9da8553b6d8
kelvimutai <kelvimutai@gmail.com>
2023-10-10 23:12:30 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar\n";
+
+	write(1, str, sizeof(str) - 1);
+	return (0);
+}
