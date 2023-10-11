06f3fddc7ff07c651c9170b1eafa20de7de9749a
kelvimutai <kelvimutai@gmail.com>
2023-10-11 13:58:43 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
-#include "unistd.h"
 #include "main.h"
 
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 _putchar('H');
