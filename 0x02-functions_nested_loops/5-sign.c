dfa9fedebb510366e8a5c9369dc58c396461b347
kelvimutai <kelvimutai@gmail.com>
2023-10-12 07:50:51 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,35 +1,29 @@
+#include <main.h>
 #include <stdio.h>
-#include <limits.h> // For INT_MAX and INT_MIN
 
-int print_sign(int n);
-
-void test_print_sign(int n) {
-    printf("Correct output - case: print_sign(%d);\n", n);
-    int result = print_sign(n);
-    switch (result) {
-        case 1:
-            putchar('+');
-            break;
-        case 0:
-            putchar('0');
-            break;
-        case -1:
-            putchar('-');
-            break;
-        default:
-            printf("Unexpected result: %d\n", result);
+int print_sign(int n)
+{
+    if (n > 0) {
+        putchar('+');
+        return 1;
+    } else if (n == 0) {
+        putchar('0');
+        return 0;
+    } else {
+        putchar('-');
+        return -1;
     }
-    printf("\n");
 }
 
 int main() {
-    test_print_sign(98);
-    test_print_sign(0);
-    test_print_sign(0xff);
-    test_print_sign(-1);
-    test_print_sign(99);
-    test_print_sign(INT_MAX);
-    test_print_sign(INT_MIN);
+    /* Testing the print_sign function */
+    print_sign(98);
+    print_sign(0);
+    print_sign(0xff);
+    print_sign(-1);
+    print_sign(99);
+    print_sign(INT_MAX);
+    print_sign(INT_MIN);
     
     return 0;
 }
