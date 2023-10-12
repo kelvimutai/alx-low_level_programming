2ebbcd19c81298f2a38fe8ecfb0003a53837b321
kelvimutai <kelvimutai@gmail.com>
2023-10-12 08:21:38 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,16 @@
-#include <main.h>
 #include <stdio.h>
+#include <limits.h>
 
-int print_sign(int n)
-{
+int print_sign(int n) {
     if (n > 0) {
-        putchar('+');
+        printf("+");
         return 1;
-    } else if (n == 0) {
-        putchar('0');
-        return 0;
-    } else {
-        putchar('-');
+    } else if (n < 0) {
+        printf("-");
         return -1;
+    } else {
+        printf("0");
+        return 0;
     }
 }
 
-int main() {
-    /* Testing the print_sign function */
-    print_sign(98);
-    print_sign(0);
-    print_sign(0xff);
-    print_sign(-1);
-    print_sign(99);
-    print_sign(INT_MAX);
-    print_sign(INT_MIN);
-    
-    return 0;
-}
