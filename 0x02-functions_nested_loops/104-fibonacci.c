0ca77d7c1162a07e622bd02bcf7ec8782fee81a1
kelvimutai <kelvimutai@gmail.com>
2023-10-12 09:23:10 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+    int b = 2;
+    int count = 2;  // We've already got the first two numbers (1 and 2)
+
+    printf("%d, %d, ", a, b);
+
+    while (count < 98) {
+        int next = a + b;
+        a = b;
+        b = next;
+
+        count++;
+
+        if (count < 98) {
+            // Print a comma and a space for all elements except the last one
+            printf("%d, ", next);
+        } else {
+            // Print a new line for the last element
+            printf("%d\n", next);
+        }
+    }
+
+    return 0;
+}
+
