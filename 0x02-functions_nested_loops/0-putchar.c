eb9c3cf7244b0db5e394790407e015e920e3b851
kelvimutai <kelvimutai@gmail.com>
2023-10-12 06:18:12 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
+
 /**
-  * main - Entry point of my code,
-  *
-  * Return: Always 0
-  */
+ * main - Entry point of my code
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-        char str[9] = "_putchar";
-        int i = 0;
+	char str[9] = "_putchar";
+	int i = 0;
 
-        while (str[i] != '\0')
-        {
-                _putchar(str[i]);
-                i++;
-        }
-        _putchar('\n');
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 
-        return (0);
+	return (0);
 }
