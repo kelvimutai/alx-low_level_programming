3a04ef0bfb32a7106c00e5c381421054313ea511
kelvimutai <kelvimutai@gmail.com>
2023-10-16 10:29:28 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,20 +5,20 @@
 #define PASSWORD_LENGTH 12
 
 char generateRandomChar() {
-    return (rand() % 94) + 32; // Generate a random printable ASCII character
+    return (rand() % 94) + 32; /* Generate a random printable ASCII character */
 }
 
 void generateRandomPassword(char *password, int length) {
-    for (int i = 0; i < length; i++) {
+    int i; // Declare 'i' before the 'for' loop
+    for (i = 0; i < length; i++) {
         password[i] = generateRandomChar();
     }
     password[length] = '\0'; // Null-terminate the string
 }
 
 int main() {
-    srand(time(NULL)); // Seed the random number generator with the current time
-
     char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
+    srand(time(NULL)); // Seed the random number generator with the current time
 
     generateRandomPassword(password, PASSWORD_LENGTH);
     
