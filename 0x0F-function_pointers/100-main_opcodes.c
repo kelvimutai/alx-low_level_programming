801c33b88018e96eece8f378f0f53898e55f1772
kelvimutai <kelvimutai@gmail.com>
2023-11-08 01:10:42 -0800

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,40 +1,29 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
-
+#include <stdio.h>
 /**
-  * main - ...
-  * @argc: ...
-  * @argv: ...
-  *
-  * Return: ...
-  */
+ * main -it will print the opcode of it own fuction.
+ * @argc: argument counter.
+ * @argv: argument vector.
+ * Return:exit 1 ( one arg is not correct), 2 ( byte is negative.)
+ */
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
+short bytes, x;
 
-	if (argc == 2)
-	{
-		val = atoi(argv[1]);
-
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
-	}
-
-	return (0);
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
 }
-
+bytes = atoi(argv[1]);
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+printf("%02x", *((unsigned char *) (main)));
+for (x = 1; x < bytes; ++x)
+printf(" %02x", *((unsigned char *) (main + x)));
+putchar('\n');
+exit(EXIT_SUCCESS);
+}
